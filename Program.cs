2115ff753ea57817c9a49089e372778993770927
Srini Kumar <srnkmrsn@gmail.com>
2019-07-30 12:27:41 -0700

    Uploaded source files.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading;
+
+using cube_space;
+using extdevice_space;
+
+namespace main
+{
+    internal class Start
+    {
+        [STAThread]
+        private static void Main()
+        {
+            Program program = new Program();
+        }
+    }
+    partial class Program
+    {
+        private const int w = 340;
+        private const int h = 336;
+        private const int h2 = 15;
+        private string website = "https://github.com/megafauna64/Kinesis-S";
+
+        private Form form;
+        private Label titleLabel;
+        private PictureBox schemaPicBox;
+        private PictureBox cubePicBox;
+        private PictureBox externalPicBox;
+        private TextBox serialTextBox;
+        private Button githubButton;
+        private Button shutdownButton;
+        private Button relinkButton;
+
+        private Image titleImage = new Bitmap(Properties.Resources.title, 310, 102);
+        private Image schemaBothUnlinkedImage = new Bitmap(Properties.Resources.both_unlinked, w, h);
+        private Image schemaBothLinkedImage = new Bitmap(Properties.Resources.both_linked, w, h);
+        private Image schemaCubeUnlinkedImage = new Bitmap(Properties.Resources.cube_unlinked, w, h);
+        private Image schemaExternalUnlinkedImage = new Bitmap(Properties.Resources.external_unlinked, w, h);
+        private Image schemaLoadImage = new Bitmap(Properties.Resources.update, w, h);
+        private Image cubeStatusOnImage = new Bitmap(Properties.Resources.cube_on, w, h);
+        private Image cubeStatusOffImage = new Bitmap(Properties.Resources.cube_off, w, h);
+        private Image externalStatusImage = new Bitmap(Properties.Resources.input_signal, w, h);
+        private Image emptyImage;
+        private Image githubButtonImage = new Bitmap(Properties.Resources.info_button, 67, h2);
+        private Image shutdownButtonImage = new Bitmap(Properties.Resources.shutdown_button, 61, h2);
+        private Image shutdownButtonLoadImage = new Bitmap(Properties.Resources.shutdown_load, 61, h2);
+        private Image relinkButtonImage = new Bitmap(Properties.Resources.relink_button, 40, h2);
+        private Image relinkButtonLoadImage = new Bitmap(Properties.Resources.relink_load, 40, h2);
+
+        Progress<Image> schemaProgress;
+        Progress<Image> cubeProgress;
+        Progress<Image> externalProgress;
+
+        private bool mouseDown;
+        private Point lastLocation;
+
+        private Cube kCube = new Cube();
+        private ExternalDevice extDevice = new ExternalDevice();
+        CancellationTokenSource source;
+
+        public Program()
+        {
+            CreateEmptyImage(w,h);
+            InitializeForm();
+            SchemaUpdate(schemaProgress);
+            InitializePolling();
+            form.ShowDialog();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Kinesis S")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("California State University San Bernardino")]
+[assembly: AssemblyProduct("Kinesis S")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ed365876-c1be-4659-b3b3-906593118082")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
